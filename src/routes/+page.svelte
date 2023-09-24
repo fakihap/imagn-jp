1fa1d2ae14093bfebc4fe4856875670501912bb1
fakihap <fakih.anugerah@gmail.com>
2023-09-24 17:35:33 +0700

    chore: made hero its own component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,8 @@
 	// import Counter from './Counter.svelte';
 	// import welcome from '$lib/images/svelte-welcome.webp';
 	// import welcome_fallback from '$lib/images/svelte-welcome.png';
-	import hero_bg from '$lib/images/hero.jpg';
-
+	
+	import Hero from "./Hero.svelte";
 </script>
 
 <svelte:head>
@@ -12,13 +12,7 @@
 </svelte:head>
 
 <section>
-	<hero>
-		<img id="hero-bg" src={hero_bg} alt="background hero"/>
-	</hero>
-	<div class="hero-headline">
-		<div id="headline">Nature speaks.</div>
-		<div>a personal library</div>
-	</div>
+	<Hero />
 </section>
 
 <style>
@@ -47,32 +41,4 @@
 		height: 0;
 		padding: 0 0 calc(100% * 495 / 2048) 0;
 	}
-
-	hero {
-		width: 100vw;
-		height: 100vh;
-	}
-
-	#hero-bg {
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
-
-		/* object-position: 0 0rem; */
-	}
-
-	.hero-headline {
-		position: absolute;
-		top: 65vh;
-		left: 15vw;
-		color: var(--color-text-white);
-		text-shadow: black 0px 2px 10px;
-		font-size: 1.4rem;
-	}
-
-	.hero-headline #headline {
-		font-size: var(--font-size-headline);
-		letter-spacing: .8rem;
-	}
-
 </style>
---
+++ src/routes/Hero.svelte
@@ -0,0 +1,40 @@
+<script>
+    import hero_bg from '$lib/images/hero.jpg';
+</script>
+
+<div class="hero">
+    <div>
+        <img id="hero-bg" src={hero_bg} alt="background hero"/>
+    </div>
+    <div class="hero-headline">
+        <div id="headline">Nature speaks.</div>
+        <div>a personal library</div>
+    </div>
+</div>
+
+<style>
+    
+
+	#hero-bg {
+		width: 100vw;
+		height: 100vh;
+		object-fit: cover;
+
+		/* object-position: 0 0rem; */
+	}
+
+	.hero-headline {
+		position: absolute;
+		top: 65vh;
+		left: 15vw;
+		color: var(--color-text-white);
+		text-shadow: black 0px 2px 10px;
+		font-size: 1.4rem;
+	}
+
+	.hero-headline #headline {
+		font-size: var(--font-size-headline);
+		letter-spacing: .8rem;
+	}
+
+</style>
