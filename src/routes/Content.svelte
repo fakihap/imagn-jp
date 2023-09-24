e11434ec00d0d975ac7c7819d22c831e21826453
fakihap <fakih.anugerah@gmail.com>
2023-09-24 21:09:38 +0700

    feat: page 3 content

--- src/routes/Content.svelte
+++ src/routes/Content.svelte
@@ -2,6 +2,10 @@
    import kyoto_1 from '$lib/images/tianshu-liu-SBK40fdKbAg-unsplash.jpg';
    import kyoto_2 from '$lib/images/redd-f-wPMvPMD9KBI-unsplash.jpg';
    import kyoto_3 from '$lib/images/sorasak-_UIN-pFfJ7c-unsplash.jpg';
+
+   import tokyo_1 from '$lib/images/yoav-aziz-tKCd-IWc4gI-unsplash.jpg';
+   import tokyo_2 from '$lib/images/vista-wei-OiERUvVrioU-unsplash.jpg';
+   
 </script>
 
 <div>
@@ -11,17 +15,16 @@
         <img id="img3"src={kyoto_3} alt="kyoto3" />
         <div>EXPLORE KYOTO</div>
     </div>
+
+    <div class="collage2">
+        <img id="img4" src={tokyo_1} alt="tokyo1" />
+        <img id="img5"src={tokyo_2} alt="tokyo2" />
+        <div>LOST IN <br/> TOKYO</div>
+    </div>
 </div>
 
 
 <style>
-    #img1, 
-    #img2, 
-    #img3 {
-        object-fit: contain;
-        position: absolute;
-    }
-
     .collage1 {
         position: absolute;
         left: 0;
@@ -41,6 +44,37 @@
         letter-spacing: .5rem;
     }
 
+    .collage2 {
+        width: 100vw;
+        height: 140vh;
+
+        position: absolute;
+        left: 0;
+        top: 250vh;
+
+        background-color: var(--color-theme-2);
+    }
+
+    .collage2 div {
+        position: relative;
+        top: 70vh;
+        /* left: 50vw; */
+        text-align: center;
+
+        font-size: 3rem;
+        color: var(--color-text-white);
+        letter-spacing: .5rem;
+    }
+
+    #img1, 
+    #img2, 
+    #img3,
+    #img4,
+    #img5 {
+        object-fit: contain;
+        position: absolute;
+    }
+
     #img1 {
         width: 50vw;
         left: 0;
@@ -52,9 +86,22 @@
         left: 50vw;
         top: 10vh;   
     }
+    
     #img3 {
         width: 50vw;        
         left: 30vw;
         top: 80vh;
+    }
+
+    #img4 {
+        width: 55vw;
+        left: 0;
+        top: 20vh;
+    }
+
+    #img5 {
+        width: 45vw;
+        left: 55vw;
+        top: 25vh;
     }
 </style>
